8bd2a1f6a1f9d9aecaa6bdd06d3fd333ff98ec54
gamma2653 <christopher2653@gmail.com>
2020-03-23 17:42:58 -0400

    Halfway through parse, and fixed build and install script

--- build_and_install.bat
+++ build_and_install.bat
@@ -1,2 +1,7 @@
-python setup.py sdist
-pip install ./dist/*.tar.gz
+@ECHO OFF
+python3 setup.py sdist
+pip3 install ./dist/service_journal-2.0a0.tar.gz
+ECHO.
+ECHO.
+ECHO Done packaging and installing!
+timeout /t 1 > NUL
--- service_journal/sql_handler/connection.py
+++ service_journal/sql_handler/connection.py
@@ -5,185 +5,177 @@ import os
 import sys
 from  service_journal.dbt_classifications import dbt_classes
 
-def init_config():
-	init = {
-		"settings": {
-			"driver": "{ODBC Driver 11 for SQL Server}",
-			"host": "AVAILDEV",
-			"username": "",
-			"password": "",
-			"__comment": "Warning: Stored passwords are unencrypted.",
-			"dbt_sql_map": {
-				"actual": {
-					"date": {
-						"name": "service_day",
-						"view": "v_vehicle_history",
-						"nullable": True
-					},
-					"blockNumber": {
-						"name": "block",
-						"view": "v_vehicle_history",
-						"nullable": True,
-					},
-					"tripNumber": {
-						"name": "trip26",
-						"view": "v_vehicle_history",
-						"nullable": True
-					},
-					"bus": {
-						"name": "bus",
-						"view": "v_vehicle_history",
-						"nullable": True
-					},
-					"time": {
-						"name": "Time",
-						"view": "v_vehicle_history",
-						"nullable": True
-					},
-					# "dep_time": {
-					# 	"name": "Departure_Time",
-					# 	"view": "v_vehicle_history",
-					# 	"nullable": True
-					# },
-					"route": {
-						"name": "Route",
-						"view": "v_vehicle_history",
-						"nullable": True
-					},
-					"dir": {
-						"name": "dir",
-						"view": "v_vehicle_history",
-						"nullable": True
-					},
-					"stop": {
-						"name": "Stop_Id",
-						"view":"v_vehicle_history",
-						"nullable": True
-					},
-					"name": {
-						"name": "Stop_Name",
-						"view": "v_vehicle_history",
-						"nullable": True
-					},
-					"boards": {
-						"name": "Boards",
-						"view": "v_vehicle_history",
-						"nullable": True
-					},
-					"alights": {
-						"name": "Alights",
-						"view": "v_vehicle_history",
-						"nullable": True
-					},
-					"onboard": {
-						"name": "Onboard",
-						"view": "v_vehicle_history",
-						"nullable": True
-					},
-					"opStatus": {
-						"name": "OperationalStatus",
-						"view": "v_vehicle_history",
-						"nullable": True
-					}
+INIT = {
+	'settings': {
+		'driver': '{ODBC Driver 11 for SQL Server}',
+		'host': 'AVAILDEV',
+		'username': '',
+		'password': '',
+		'__comment': 'Warning: Stored passwords are unencrypted.',
+		'dbt_sql_map': {
+			'actual': {
+				'date': {
+					'name': 'service_day',
+					'view': 'v_vehicle_history',
+					'nullable': True
 				},
-				"scheduled": {
-					"date": {
-						"name": "calendar",
-						"view": "v_sched_trip_stop",
-						"nullable": True
-					},
-					"blockNumber": {
-						"name": "BlockNumber",
-						"view": "v_sched_trip_stop",
-						"nullable": True
-					},
-					"dir": {
-						"name": "Direction",
-						"view": "v_sched_trip_stop",
-						"nullable": True
-					},
-					"tripNumber": {
-						"name": "Trip26",
-						"view": "v_sched_trip_stop",
-						"nullable": True
-					},
-					"i_stop": {
-						"name": "iStop",
-						"view": "v_sched_trip_stop",
-						"nullable": True
-					},
-					"stop": {
-						"name": "tStop",
-						"view": "v_sched_trip_stop",
-						"nullable": True
-					},
-					"i_stop_name": {
-						"name": "iStopName",
-						"view": "v_sched_trip_stop",
-						"nullable": True
-					},
-					"t_stop_name": {
-						"name": "tStopName",
-						"view": "v_sched_trip_stop",
-						"nullable": True
-					},
-					"time": {
-						"name": "DepartureTime",
-						"view": "v_sched_trip_stop",
-						"nullable": True
-					},
-					"layover": {
-						"name": "layover",
-						"view": "v_sched_trip_stop",
-						"nullable": True
-					},
-					"run": {
-						"name": "RunNumber",
-						"view": "v_sched_trip_stop",
-						"nullable": True
-					},
-					"pieceNumber": {
-						"name": "PieceNumber",
-						"view": "v_sched_trip_stop",
-						"nullable": True
-					}
+				'blockNumber': {
+					'name': 'block',
+					'view': 'v_vehicle_history',
+					'nullable': True,
 				},
-				"views": {
-					"v_sched_trip_stop": {
-						"deflt_query": "SELECT [%(date)s],[%(blockNumber)s],[%(dir)s],"+
-						"[%(tripNumber)s],[%(i_stop)s],[%(t_stop)s],[%(i_stop_name)s],"+
-						"[%(t_stop_name)s],[%(time)s],[%(layover)s],[%(run)s],[%(pieceNumber)s]"+
-	  					"FROM [%(table)s] "+
-						"WHERE %(searchParam)s=%(search)s"
-					},
-					"v_vehicle_history": {
-						"deflt_query": "SELECT [%(date)s], [%(blockNumber)s], "+
-						"[%(tripNumber)s], [%(bus)s], [%(time)s], [%(route)s], "+
-						"[%(dir)s], [%(stop)s], [%(name)s], [%(boards)s], "+
-						"[%(alights)s], [%(onboard)s], [%(opStatus)s]"+
-						"FROM [%(table)s] "+
-						"WHERE %(searchParam)s=%(search)s"
-					}
+				'tripNumber': {
+					'name': 'trip26',
+					'view': 'v_vehicle_history',
+					'nullable': True
 				},
-				"databases": {
-					"scheduled": "TA_ITHACA_SCHEDULE_HISTORY",
-					"actual": "TA_ITHACA_ACTUAL_HISTORY",
+				'bus': {
+					'name': 'bus',
+					'view': 'v_vehicle_history',
+					'nullable': True
+				},
+				'time': {
+					'name': 'Time',
+					'view': 'v_vehicle_history',
+					'nullable': True
+				},
+				# 'dep_time': {
+				# 	'name': 'Departure_Time',
+				# 	'view': 'v_vehicle_history',
+				# 	'nullable': True
+				# },
+				'route': {
+					'name': 'Route',
+					'view': 'v_vehicle_history',
+					'nullable': True
+				},
+				'dir': {
+					'name': 'dir',
+					'view': 'v_vehicle_history',
+					'nullable': True
+				},
+				'stop': {
+					'name': 'Stop_Id',
+					'view':'v_vehicle_history',
+					'nullable': True
+				},
+				'name': {
+					'name': 'Stop_Name',
+					'view': 'v_vehicle_history',
+					'nullable': True
+				},
+				'boards': {
+					'name': 'Boards',
+					'view': 'v_vehicle_history',
+					'nullable': True
+				},
+				'alights': {
+					'name': 'Alights',
+					'view': 'v_vehicle_history',
+					'nullable': True
+				},
+				'onboard': {
+					'name': 'Onboard',
+					'view': 'v_vehicle_history',
+					'nullable': True
+				},
+				'opStatus': {
+					'name': 'OperationalStatus',
+					'view': 'v_vehicle_history',
+					'nullable': True
 				}
 			},
-			# "optSelectScheduledQuery": [
-			# 	"SELECT Message_Type_Id, service_date, block, route, dir, trip, vmh_time, bus, Deviation, Onboard, Boards, Alights, Stop_Id, Stop_Name, Departure_Time, Latitude, Longitude FROM dbo.v_actual_block_trip_stop WHERE service_date = ? AND block = ? ORDER BY vmh_time asc",
-			# 	"Service_Date",
-			# 	"BlockNumber"
-			# ],
-			# "selectScheduledQuery": [
-			# 	"SELECT Message_Type_Id, service_date, block, route, dir, trip, vmh_time, bus, Deviation, Onboard, Boards, Alights, Stop_Id, Stop_Name, Departure_Time, Latitude, Longitude FROM dbo.v_actual_block_trip_stop WHERE service_date = ? ORDER BY vmh_time asc",
-			# 	"Service_Date"
-			# ]
-		}
+			'scheduled': {
+				'date': {
+					'name': 'calendar',
+					'view': 'v_sched_trip_stop',
+					'nullable': True
+				},
+				'blockNumber': {
+					'name': 'BlockNumber',
+					'view': 'v_sched_trip_stop',
+					'nullable': True
+				},
+				'dir': {
+					'name': 'Direction',
+					'view': 'v_sched_trip_stop',
+					'nullable': True
+				},
+				'tripNumber': {
+					'name': 'Trip26',
+					'view': 'v_sched_trip_stop',
+					'nullable': True
+				},
+				'i_stop': {
+					'name': 'iStop',
+					'view': 'v_sched_trip_stop',
+					'nullable': True
+				},
+				'stop': {
+					'name': 'tStop',
+					'view': 'v_sched_trip_stop',
+					'nullable': True
+				},
+				'i_stop_name': {
+					'name': 'iStopName',
+					'view': 'v_sched_trip_stop',
+					'nullable': True
+				},
+				't_stop_name': {
+					'name': 'tStopName',
+					'view': 'v_sched_trip_stop',
+					'nullable': True
+				},
+				'time': {
+					'name': 'DepartureTime',
+					'view': 'v_sched_trip_stop',
+					'nullable': True
+				},
+				'layover': {
+					'name': 'layover',
+					'view': 'v_sched_trip_stop',
+					'nullable': True
+				},
+				'run': {
+					'name': 'RunNumber',
+					'view': 'v_sched_trip_stop',
+					'nullable': True
+				},
+				'pieceNumber': {
+					'name': 'PieceNumber',
+					'view': 'v_sched_trip_stop',
+					'nullable': True
+				}
+			},
+			'views': {
+				'v_sched_trip_stop': {
+					'deflt_query': ('SELECT %(date)s,%(blockNumber)s,%(dir)s,'
+					'%(tripNumber)s,%(i_stop)s,%(t_stop)s,%(i_stop_name)s,'
+					'%(t_stop_name)s,%(time)s,%(layover)s,%(run)s,%(pieceNumber)s'
+					'FROM %(table)s '
+					'WHERE %(searchParam)s=%(search)s')
+				},
+				'v_vehicle_history': {
+					'deflt_query': ('SELECT %(date)s, %(blockNumber)s, '
+					'%(tripNumber)s, %(bus)s, %(time)s, %(route)s, '
+					'%(dir)s, %(stop)s, %(name)s, %(boards)s, '
+					'%(alights)s, %(onboard)s, %(opStatus)s'
+					'FROM %(table)s '
+					'WHERE %(searchParam)s=%(search)s')
+				}
+			},
+			'databases': {
+				'scheduled': 'TA_ITHACA_SCHEDULE_HISTORY',
+				'actual': 'TA_ITHACA_ACTUAL_HISTORY',
+			}
+		},
 	}
+}
 
+
+def init_config():
 	f = open('config.json', 'w')
-	f.write(json.dumps(init, sort_keys=True, indent=4))
+	f.write(json.dumps(INIT, sort_keys=True, indent=4))
 	f.close()
 
 def read_config():
@@ -193,38 +185,7 @@ def read_config():
 	del f
 	return config
 
-
-# Open or initialize config into dictionary
-if not os.path.exists('config.json'):
-	init_config()
-
-config = read_config()
-try:
-	settings = config['Settings']
-except KeyError as e:
-	print(('Error: Key (%s) not found in config.json. If this is your first time '
-	'running this, please setup your config and re-run it.') % (e.args[0]))
-	raise e
-
-dbt_sql_map = settings['dbt_sql_map'] #load in json
-sql_dbt_map={}
-for t in dbt_sql_map:
-	if not t in sql_dbt_map:
-		sql_dbt_map[t] = {}
-	if t=='actual' or t=='scheduled':
-		for field in dbt_sql_map[t]:
-			sql_dbt_map[t][dbt_sql_map[t][field]['name']] = {'name':field, 'nullable':dbt_sql_map[t][field]['nullable'], 'view':dbt_sql_map[t][field]['view']}
-	else:
-		sql_dbt_map[t]=dbt_sql_map[t]
-# TEMP:
-# import pprint
-# pprint.pprint(dbt_sql_map)
-
-# sql_dbt_map = {'Scheduled' : {value['name'] : {'name' : key, 'nullable' : value['nullable'], 'view' : value['view']} for (key, value) in dbt_sql_map['Scheduled'].items()}, 'Actual' : {value['name'] : {'name' : key, 'nullable' : value['nullable'], 'view' : value['view']} for (key, value) in dbt_sql_map['Actual'].items()}}
-# pprint.pprint(sql_dbt_map)
-# Still not working
-# sql_dbt_map = {exist: {value if key=='Database' else value['name'] : {'name' : key, 'nullable' : value['nullable'], 'view' : value['view']} for (key, value) in items.items()} for (exist, items) in dbt_sql_map.items()}
-class TCATConnection:
+class Connection:
 	def close(self):
 		try:
 			self.db.close()
@@ -234,24 +195,58 @@ class TCATConnection:
 		self.close()
 		self.super().__del__()
 
+	def __init__(self, config_path):
+		# Open or initialize config into dictionary
+		if not os.path.exists(config_path, 'config.json'):
+			init_config()
+
+		config = read_config()
+		try:
+			settings = config['Settings']
+		except KeyError as e:
+			print(('Error: Key (%s) not found in config.json. If this is your first time '
+			'running this, please setup your config and re-run it.') % (e.args[0]))
+			raise e
+
+		dbt_sql_map = settings['dbt_sql_map'] #load in json
+		sql_dbt_map={}
+		for t in dbt_sql_map:
+			if not t in sql_dbt_map:
+				sql_dbt_map[t] = {}
+			if t=='actual' or t=='scheduled':
+				for field in dbt_sql_map[t]:
+					sql_dbt_map[t][dbt_sql_map[t][field]['name']] = {'name':field, 'nullable':dbt_sql_map[t][field]['nullable'], 'view':dbt_sql_map[t][field]['view']}
+			else:
+				sql_dbt_map[t]=dbt_sql_map[t]
+		self.dbt_sql_map = dbt_sql_map
+		self.sql_dbt_map = sql_dbt_map
+		self.resetConnection(config)
+
 	def resetConnection(self, config):
 		try:
 			self.driver = config['driver']
 			self.user = config['username']
 			self.password = config['password']
-			self.databases = config['databases']
-			self.host = config['Host']
+			self.databases = self.dbt_sql_map['databases']
+			self.host = config['host']
 		except KeyError as e:
 			print(('Error: Key (%s) not found in config.json. If this is your first time '
 				   'running this, please setup your config and re-run it.') % (e.args[0]))
 			raise e
-		self.read_conn = pyodbc.connect(r'DRIVER=%s;'
+		self.actual_read_conn = pyodbc.connect(r'DRIVER=%s;'
 			#The host driver, as list of these can be found on the pyodbc
 			#library readme on github
 			r'SERVER=%s;'
 			r'DATABASE=%s;'
 			r'UID=%s;'
-			r'PWD=%s'% (self.driver, self.host, self.read_database, self.user, self.password))
+			r'PWD=%s'% (self.driver, self.host, self.databases['actual'], self.user, self.password))
+		self.scheduled_read_conn = pyodbc.connect(r'DRIVER=%s;'
+			#The host driver, as list of these can be found on the pyodbc
+			#library readme on github
+			r'SERVER=%s;'
+			r'DATABASE=%s;'
+			r'UID=%s;'
+			r'PWD=%s'% (self.driver, self.host, self.databases['scheduled'], self.user, self.password))
 		self.write_conn = pyodbc.connect(r'DRIVER=%s;'
 			#The host driver, as list of these can be found on the pyodbc
 			#library readme on github
@@ -259,42 +254,62 @@ class TCATConnection:
 			r'DATABASE=%s;'
 			r'UID=%s;'
 			r'PWD=%s'% (self.driver, self.host, self.write_database, self.user, self.password))
-	def __init__(self, config):
-		self.resetConnection(config)
 
 	# Only should be called on
 	def selectActualDate(self, date, block=-1):
 		if block==-1:
-			query = settings['selectScheduledQuery']
+			query = settings['dbt_sql_map']['views']['actual']
 		else:
-			query = settings['optSelectScheduledQuery']
+			query = settings['dbt_sql_map']['views']['actual']
+		# else:
+		# 	query = settings['optSelectScheduledQuery']
 		cursor = self.read_conn.cursor()
 		cursor.execute(query, date, block)
 		return cursor
+
 	def loadData(self, cursor, day=None):
 		cursor = self.cursor
 		retday = None
-		if day:
-			pass
-		else:
-			row = cursor.fetchone()
-			# cursor_description documentation:
-			# 0. column name (or alias, if specified in the SQL)
-			# 1. type code
-			# 2. display size (pyodbc does not set this value)
-			# 3. internal size (in bytes)
-			# 4. precision
-			# 5. scale
-			# 6. nullable (True/False)
-			col_names = row.cursor_description[0]
-			nullable = row.cursor_description[6]
-			print(col_names)
-			print(nullable)
-			while row:
 
-				# Have to login to avl to do some testing to see if col_names is
-				# a tuple of column names
-				row = cursor.fetchone()
+		row = cursor.fetchone()
+		# cursor_description documentation:
+		# 0. column name (or alias, if specified in the SQL)
+		# 1. type code
+		# 2. display size (pyodbc does not set this value)
+		# 3. internal size (in bytes)
+		# 4. precision
+		# 5. scale
+		# 6. nullable (True/False)
+		col_names = cursor.description[0]
+		nullable = cursor.description[6]
+		dbt_col_names = [sql_dbt_map[col] for col in col_names]
+		print(col_names)
+		print(dbt_col_names)
+		print(nullable)
+		while row:
+			# We zip up our data making a key-value pairing oc col_names and rows
+			data = dict(zip(dbt_col_names, row))
+			if not day:
+				day = dbt_classes.Day()
+			day.date = data['date']
+			# This would be for scheduled
+			# try:
+			# 	block = day.getBlock(data['blockNumber'])
+			# except BlockNotFound:
+			# 	block = dbt_classes.Block(data['blockNumber'])
+			# 	day.addBlock(block)
+			try:
+				bus = day.getBus(data['bus'])
+			except BusNotFound:
+				bus = dbt_classes.Bus(data('bus'))
+				day.addBus(bus)
+			try:
+				trip = bus.getTrip(data['tripNumber'])
+			except TripNotFound:
+				trip = dbt_classes.Trip(data['tripNumber'], seq, data['route'], data['dir'])
+			# Have to login to avl to do some testing to see if col_names is
+			# a tuple of column names
+			row = cursor.fetchone()
 	# [actualDay] is an ActualDay being written to the database
 	def writeToRebuiltSegments(actualDay, connection):
 		cursor = self.db.cursor()
