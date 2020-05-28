c498e78e87b722865820a6854b862348eecae51b
gamma2653 <christopher2653@gmail.com>
2020-05-28 14:20:24 -0400

    Fixed dictionary initialization

--- service_journal/dbt_classifications/dbt_classes.py
+++ service_journal/dbt_classifications/dbt_classes.py
@@ -38,7 +38,7 @@ class Days:
 			self.addDay(date)
 		if blockNumber not in self.root[date]:
 			self.addBlock(date, blockNumber)
-		self.root[date][blockNumber][tripNumber] = dict({'stops':OrderedDict(), 'route':route, 'direction':direction})
+		self.root[date][blockNumber][tripNumber] = OrderDict({'stops':OrderedDict(), 'route':route, 'direction':direction, 'actual_start':None, 'actual_end':None, 'flag'=Flag.FINE})
 	# stopInfo is a tuple containing stopInfo
 	def addStop(self, date, blockNumber, tripNumber, route, direction, stopID, stopName, sched_time, distance,):
 		if date not in self.root:
@@ -56,36 +56,43 @@ class Days:
 			instance_id+=1
 		stops[(stopID, instance_id)] = dict({'name':stopName,\
 		 'sched_time':sched_time, 'actual_time':None,'distance':distance, 'bus':None,\
-		 'avl_boards', 'avl_alights', 'avl_onboard', 'boards':0, 'alights':0, \
+		 'avl_boards':0, 'avl_alights':0, 'avl_onboard':0, 'boards':0, 'alights':0, \
 		 'onboard':0, 'adjustedOnboard':None, 'seen':0, 'flag':Flag.FINE})
 
 
 	def crossRef(self, date, blockNumber, tripNumber, stopID, bus, boards, alights, onboard, actual_time, loc, route, stop_locations):
 		if date in self.root:
-			if blockNumber in self.root[date]:
-				if tripNumber in self.root[date][blockNumber]:
+			day = self.root[date]
+			if blockNumber in day:
+				block = day[blockNumber]
+				if tripNumber in block:
+					trip = block[tripNumber]
+					stops = trip['stops']
 					if stopID==0:
 						t_stop_locs = {stop_id : stop_locations[stop_id] for \
-						(stop_id, instance_id) in self.root[date][blockNumber][tripNumber]['stops'].keys()}
+						(stop_id, instance_id) in stops.keys()}
 						stopID = closestStopID(stop_locations, loc)
 						was197 = True
 					else:
 						was197 = False
-					stops = self.root[date][blockNumber][tripNumber]['stops']
 					instance_id = 0
 					(l_stopID, _), l_stop_info = stops.items()[0]
 					if stopID==l_stopID and l_stop_info['seen']:
 						instance_id+=1
 					# while stops[(stopID, instance_id)].seen==10 and not was197:
 					# 	instance_id+=1
-					if stopID in self.root[date][blockNumber][tripNumber]['stops']:
-						stop = self.root[date][blockNumber][tripNumber]['stops'][(stopID, instance_id)]
+					if stopID in stops:
+						stop = stops[(stopID, instance_id)]
 						stop['bus'] = bus
 						stop['avl_boards'] = stop['avl_boards']+boards
 						stop['avl_alights'] = stop['avl_alights']+alights
 						stop['avl_onboard'] = stop['avl_onboard']+onboard
 						stop['actual_time'] = actual_time
 						stop['seen']+= 1 if was197 else 10
+						if not trip['actual_start'] or actual_time<trip['actual_start']:
+							trip['actual_start'] = actual_time
+						if not trip['actual_end'] or actual_time>trip['actual_end']:
+							trip['actual_end'] = actual_time
 	# def postProcess(self):
 	# 	buses = {}
 	# 	for date in self.root.keys():
@@ -102,26 +109,51 @@ class Days:
 	# 						if stop['bus'] not in buses:
 	# 							buses[stop['bus']] = []
 	# 						buses[stop['bus']].append(stop)
+	def flagDeviations(self):
+		for blockNumber, block in day.items():
+			busesOnBlock = set()
+			for tripNumber, trip in block.items():
+				prevStop = None
+				for stopID_instance, stop in trip['stops'].items():
+					buses.add(stop['bus'])
+					if prevStop['bus']==stop['bus'] and stop['actual_time'] < prevStop['actual_time']:
+						# We went back in time!
+						stop['flag'] |= Flag.BACKWARDS_TIME
+						prevStop['flag'] |= Flag.BACKWARDS_TIME
+					prevStop = stop
+			if len(busesOnBlock>1):
+				for trip in trip.values():
+					trip['flag'] |= Flag.MULTIPLE_BUS_BLOCK
 
 	def inferStops(self):
-		bus_start_end = {}
-		for bus, stops in self.getStopsByBusDict():
-			for stop in stops:
-				if bus not in bus_start_end:
-					bus_start_end[bus] = (stop['actual_time'], stop['actual_time'])
-				else:
-					# cross-reference bus stop times
-					if stop['actual_time']>bus_start_end[bus][0]:
-						pass
-		# for date in self.root.keys():
-		# 	for blockNumber in self.root[date].keys():
-		# 		busStops = {}
-		# 		for tripNumber in self.root[date][blockNumber].keys():
-		# 			for (stopID, instanceID), stop in self.root[date][blockNunber][tripNumber]['stops'].items():
-		# 				busStops[stop['bus']] = stop
-		# 				if not stop['bus']:
-		# 					# Try to find previous bus
+		# Organize data
+		for date, day in self.root.items()
+			for blockNumber, block in day.items():
+				# busesOnBlock = {}
+				# stopsToSort = {}
+				prevStop = None
+				for tripNumber, trip in block.items():
+					lookAhead = []
+					for stopID_instance, stop in trip['stops'].items():
+						# look back
+						if not stop['bus'] and prevStop:
+							stop['bus'] = prevStop[1]['bus']
+							stop['onboard'] = prevStop[1]['onboard']
+						# look ahead
+						else:
+							lookAhead.append(stop)
+						_, lastStop = trip['stops'].items()[len(trip['stops'])-1]
+						_, sndToLastStop = trip['stops'].items()[len(trip['stops'])-2]
+						# We determine that the end of the trip happened.
+						tripCompleted = lastStop['seen'] and sndToLastStop['seen']
+						if stop['seen'] and tripCompleted:
+							while lookAhead:
+								s = lookAhead.pop()
+								s['bus'] = stop['bus']
+								s['onboard'] = stop['onboard']
 
+							# look ahead
+						prevStop = (stopID_instance, stop)
 
 
 	def getStopsByBusDict(self):
