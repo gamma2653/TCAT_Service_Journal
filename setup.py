8cfd8377f03562beda76e04056cac05bfd7d35cf
gamma2653 <christopher2653@gmail.com>
2020-11-28 22:31:54 -0500

    Cleaned up my own setup.py to include more information.

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 from setuptools import setup, find_packages
+def readme():
+    data = ''
+    with open('README.md', 'r') as f:
+        data = f.read()
+    return data
 setup(
     name="service_journal",
-    version="2.0a",
+    author="Christopher De Jesus",
+    author_email="cd525@cornell.edu",
+    description="A framework for journaling TCAT bus activity.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    version="2.1",
     packages=find_packages(),
 )
