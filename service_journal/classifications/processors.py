a0c3f0e497ae488ab74029e76a23f47c8477f305
gamma2653 <christopher2653@gmail.com>
2021-05-05 21:11:49 -0400

    Added warning when trip_shapes are empty (shouldn't be happening).

--- service_journal/classifications/processors.py
+++ service_journal/classifications/processors.py
@@ -35,7 +35,9 @@ def prep_segment_analysis(journal):
                 tracked_intervals_db = tracked_intervals_d[block_key]
                 trip_shapes = get_shape_trip(stops, expanded_shapes)
                 if not converted_actuals[date_key][block_key][trip_key]:
-                    logger.warning('converted actuals for [%s][%s][%s] are empty!', date_key, block_key, trip_key)
+                    logger.warning('converted_actuals for [%s][%s][%s] are empty!', date_key, block_key, trip_key)
+                if not trip_shapes:
+                    logger.warning('trip_shapes for [%s][%s][%s] are empty!', date_key, block_key, trip_key)
                 tracked_intervals_db[trip_key] = track_intervals(trip_shapes, stop_locations,
                                                                  [(ping['lat'], ping['lon']) for ping in
                                                                   converted_actuals[date_key][block_key][trip_key]])
